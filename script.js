7f9c5843e2627a24d39e8ed3fd915d592e89bfa2
Ritik70k <ritik707086@gmail.com>
2026-05-30 18:22:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var typed = new Typed('#typed', {
+            strings : ['Developer', 'Web Designer', 'Problem solver'],
+            typeSpeed:90,
+            backSpeed :80,
+            backDelay:500,
+            loop : true
+        });
