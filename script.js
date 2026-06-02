fcd245e55e5d13d3d23f403e6491d6df2467e1e6
Ritik70k <ritik707086@gmail.com>
2026-06-02 13:27:25 +0530

    Fixed Web3Forms contact form and responsivness

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 
 
 var typed = new Typed('#typed', {
-            strings : ['Developer', 'Web Designer', 'Problem solver'],
-            typeSpeed:90,
-            backSpeed :80,
-            backDelay:500,
-            loop : true
-        });
+    strings: ['Developer', 'Web Designer', 'Problem solver'],
+    typeSpeed: 90,
+    backSpeed: 80,
+    backDelay: 500,
+    loop: true
+});
+
+// Mobile Navbar Toggle
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Auto close menu after click
+
+document.querySelectorAll("#navLinks a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
